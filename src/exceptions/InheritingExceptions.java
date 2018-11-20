11dcd9c22b026dbc68f4e08f698a6a2c314e2f59
xiezhiping <1529464401@qq.com>
2018-11-20 14:56:41 +0800

    自己定义异常类并使用的技巧

---
+++ src/exceptions/InheritingExceptions.java
@@ -0,0 +1,20 @@
+package exceptions;
+
+class SimpleException extends Exception {
+	
+}
+public class InheritingExceptions {
+	public void f() throws SimpleException {
+		System.out.println("Throw SimpleException from f()");
+		throw new SimpleException();
+	}
+	public static void main(String[] args) {
+		InheritingExceptions sed = new InheritingExceptions();
+		try {
+			sed.f();
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.getMessage();
+		}
+	}
+}
